040fb51a68366cf51792ea77893d42b3a9ebe4cd
Edgar Gonzalez <edgargonzalez@gmail.com>
2018-04-27 20:04:16 -0400

    WIP

--- nycgeoclient.go
+++ nycgeoclient.go
@@ -1,35 +1,121 @@
 package nycgeoclient
 
 import (
-	"encoding/json"
-	"io"
+	// "encoding/json"
+	// "io"
+	"fmt"
+	"net/http"
 	"net/url"
+	"strings"
+)
+
+type ResponseType int
+
+const (
+	JSON ResponseType = 1 + iota
+	XML
 )
 
 const (
-	libraryVersion = "0.0.1"
-	defaultBaseURL = "https://api.cityofnewyork.us/geoclient/v1/"
-	userAgent      = "go-nycgeoclient/" + libraryVersion
-	format         = "json"
+	libraryVersion   = "0.0.1"
+	defaultBaseURL   = "https://api.cityofnewyork.us/geoclient/v1/"
+	defaultUserAgent = "go-nycgeoclient/" + libraryVersion
+	defaulFormat     = JSON
 )
 
-/ Client manages communication with NYC Geoclient V1 API.
+type Config struct {
+	HttpClient *http.Client
+
+	// Base URL for API requests.
+	BaseURL string
+
+	// App ID
+	AppID string
+
+	// App key
+	AppKey string
+
+	// Format
+	Format ResponseType
+
+	// User Agent
+	UserAgent string
+}
+
+// Client manages communication with NYC Geoclient V1 API.
 type Client struct {
-  // HTTP client used to communicate with the NYC Geoclient API.
-  client *http.Client
+	// HTTP client used to communicate with the NYC Geoclient API.
+	client *http.Client
 
-  // Base URL for API requests.
-  BaseURL *url.URL
+	// Base URL for API requests.
+	BaseURL *url.URL
 
-  // User agent for client
-  UserAgent string
+	// User agent for client
+	UserAgent string
 
-  // App ID
-  AppId string
+	// App ID
+	AppID string
 
-  // App key
-  AppKey string
+	// App key
+	AppKey string
 
-  // Format (json or xml)
-  Format string
+	// Format
+	Format ResponseType
+}
+
+// NewClient returns a new NYC GeoClient API client.
+func NewClient(config Config) (*Client, error) {
+	httpClient := config.HttpClient
+	if httpClient == nil {
+		httpClient = http.DefaultClient
+	}
+	baseURL := config.BaseURL
+	if baseURL == "" {
+		baseURL = defaultBaseURL
+	}
+	baseEndpoint, err := url.Parse(baseURL)
+	if err != nil {
+		return nil, err
+	}
+	if !strings.HasSuffix(baseEndpoint.Path, "/") {
+		baseEndpoint.Path += "/"
+	}
+	format := config.Format
+	if format == 0 {
+		format = defaulFormat
+	}
+	userAgent := config.UserAgent
+	if userAgent == "" {
+		userAgent = defaultUserAgent
+	}
+	c := &Client{client: httpClient, BaseURL: baseEndpoint, UserAgent: userAgent, AppID: config.AppID, AppKey: config.AppKey, Format: format}
+
+	return c, nil
+}
+
+// NewRequest creates an API request. A relative URL can be provided in urlStr,
+// in which case it is resolved relative to the BaseURL of the Client.
+// It only accepts relative URLs, if the URL provided starts with an "/" the first "/" will be removed.
+// The NYC GeoClient API is read-only, no need to send a body
+func (c *Client) NewRequest(method, urlStr string) (*http.Request, error) {
+	if !strings.HasSuffix(c.BaseURL.Path, "/") {
+		return nil, fmt.Errorf("BaseURL must have a trailing slash, but %q does not", c.BaseURL)
+	}
+	if strings.HasPrefix(urlStr, "/") {
+		urlStr = urlStr[1:]
+	}
+	u, err := c.BaseURL.Parse(urlStr)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest(method, u.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if c.UserAgent != "" {
+		req.Header.Set("User-Agent", c.UserAgent)
+	}
+	return req, nil
 }
---
+++ nycgeoclient_test.go
@@ -0,0 +1,91 @@
+package nycgeoclient
+
+import (
+	"testing"
+)
+
+func TestNewClient(t *testing.T) {
+	c, _ := NewClient(Config{})
+
+	if got, want := c.BaseURL.String(), defaultBaseURL; got != want {
+		t.Errorf("NewClient BaseURL is \"%v\", want \"%v\"", got, want)
+	}
+	if got, want := c.UserAgent, defaultUserAgent; got != want {
+		t.Errorf("NewClient UserAgent is \"%v\", want \"%v\"", got, want)
+	}
+	if got, want := c.Format, defaulFormat; got != want {
+		t.Errorf("NewClient Format is \"%v\", want \"%v\"", got, want)
+	}
+	if got, want := c.AppID, ""; got != want {
+		t.Errorf("NewClient AppID is \"%v\", want \"%v\"", got, want)
+	}
+	if got, want := c.AppKey, ""; got != want {
+		t.Errorf("NewClient AppKey is \"%v\", want \"%v\"", got, want)
+	}
+}
+
+func TestNewClient_withCustomValues(t *testing.T) {
+	testURL := "http://test.com/path/"
+	testUserAgent := "dumb-1.0"
+	testAppID := "foo"
+	testAppKey := "bar"
+	testFormat := XML
+
+	c, _ := NewClient(Config{BaseURL: testURL, UserAgent: testUserAgent, AppID: testAppID, AppKey: testAppKey, Format: testFormat})
+
+	if got, want := c.BaseURL.String(), testURL; got != want {
+		t.Errorf("NewClient BaseURL is \"%v\", want \"%v\"", got, want)
+	}
+	if got, want := c.UserAgent, testUserAgent; got != want {
+		t.Errorf("NewClient UserAgent is \"%v\", want \"%v\"", got, want)
+	}
+	if got, want := c.Format, testFormat; got != want {
+		t.Errorf("NewClient Format is \"%v\", want \"%v\"", got, want)
+	}
+	if got, want := c.AppID, testAppID; got != want {
+		t.Errorf("NewClient AppID is \"%v\", want \"%v\"", got, want)
+	}
+	if got, want := c.AppKey, testAppKey; got != want {
+		t.Errorf("NewClient AppKey is \"%v\", want \"%v\"", got, want)
+	}
+}
+
+func TestNewClient_withURLWithoutTrailingSlash(t *testing.T) {
+	testURL := "http://test.com/path"
+	formattedTestURL := testURL + "/"
+
+	c, _ := NewClient(Config{BaseURL: testURL})
+
+	if got, want := c.BaseURL.String(), formattedTestURL; got != want {
+		t.Errorf("NewClient BaseURL is \"%v\", want \"%v\"", got, want)
+	}
+}
+
+func TestNewRequest(t *testing.T) {
+	c, _ := NewClient(Config{})
+
+	inURL, outURL := "foo", defaultBaseURL+"foo"
+	req, _ := c.NewRequest("GET", inURL)
+
+	// test that relative URL was expanded
+	if got, want := req.URL.String(), outURL; got != want {
+		t.Errorf("NewRequest(%q) URL is %v, want %v", inURL, got, want)
+	}
+
+	// test that default user-agent is attached to the request
+	if got, want := req.Header.Get("User-Agent"), c.UserAgent; got != want {
+		t.Errorf("NewRequest() User-Agent is %v, want %v", got, want)
+	}
+}
+
+func TestNewRequest_withAbsoluteURL(t *testing.T) {
+	c, _ := NewClient(Config{})
+
+	inURL, outURL := "/foo", defaultBaseURL+"foo"
+	req, _ := c.NewRequest("GET", inURL)
+
+	// test that absolute URL is handled as a relative one
+	if got, want := req.URL.String(), outURL; got != want {
+		t.Errorf("NewRequest(%q) URL is %v, want %v", inURL, got, want)
+	}
+}
